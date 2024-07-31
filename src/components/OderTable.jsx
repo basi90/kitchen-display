a769a6b3a12df877159fdd758e44c5cfa3fd4e60
Andrea Basili <andrea.basili@live.it>
2024-07-31 19:23:07 +0200

    Create OrderTable component

---
+++ src/components/OderTable.jsx
@@ -0,0 +1,16 @@
+// src/components/OrderTable.js
+import React from 'react';
+import Order from './Order';
+
+function OrderTable({ tableName, orders }) {
+  return (
+    <div className="order-table">
+      <h2>Table: {tableName}</h2>
+      {Object.entries(orders).map(([product, quantity]) => (
+        <Order key={product} product={product} quantity={quantity} />
+      ))}
+    </div>
+  );
+}
+
+export default OrderTable;
