7bc4255210d12ecaebc9d478fcd5f0cbe03b6d4c
Andrea Basili <andrea.basili@live.it>
2024-08-01 15:01:03 +0200

    fixing ui

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OrderTable from '../OrderTable/OrderTable';
 import './Table.css';
 
 function Table({ data }) {
+  const [highlightedId, setHighlightedId] = useState(null);
+
+  const handleHighlightChange = (id) => {
+    setHighlightedId(id);
+  };
+
   return (
     <div className="table">
-      {Object.entries(data).map(([tableName, orders]) => (
+      {Object.entries(data).map(([tableName, orders], index) => (
         <OrderTable
           key={tableName}
           tableName={tableName}
           orders={orders}
-          tableId="123"
+          tableId={tableName} // Assuming tableName is unique and can be used as id
           serverName="John Doe"
           timePassed="15 mins"
           initialStatus="new"
           peopleCount={4}
+          isHighlighted={tableName === highlightedId}
+          onHighlightChange={handleHighlightChange}
         />
       ))}
     </div>
